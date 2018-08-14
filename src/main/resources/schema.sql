85f893cbb8ed347c5abef6d20ab6db48f07d0724
limpeh <yusheng.ngo.2014@smu.edu.sg>
2018-08-13 22:34:19 -0400

    Commit - schema.sql
    Updated
    - application.properties

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,34 @@
+drop table users if exists;
+
+create table users(
+	userId int(11) NOT NULL AUTO_INCREMENT,
+	firstName varchar(100),
+	lastName varchar(100),
+	contact varchar(20)
+	email varchar(100),
+	role varchar(20),
+	PRIMARY KEY(userId)
+);
+
+drop table orders if exists;
+
+create table orders(
+	orderId int(11) NOT NULL AUTO_INCREMENT,
+	userId int NOT NULL,
+	tickerSymbol varchar(10),
+	side varchar(10),
+	orderType varchar(10),
+	price double,
+	noOfShares int,
+	status varchar(20),
+	orderTimeStamp varchar(50),
+	PRIMARY KEY(orderId)
+);
+
+drop table sectors if exists;
+
+create table sectors(
+	name varchar(100),
+	description varchar(200),
+	PRIMARY KEY(name)
+);
