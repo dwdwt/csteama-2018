3149bc0a8b10be6cf9aec98d76cbe07710ba82e6
dwdwt <me@dwdwt.com>
2018-08-14 06:14:04 -0400

    project set up

---
+++ src/main/java/com/cs/Csteama2018Application.java
@@ -0,0 +1,12 @@
+package com.cs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Csteama2018Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Csteama2018Application.class, args);
+	}
+}
