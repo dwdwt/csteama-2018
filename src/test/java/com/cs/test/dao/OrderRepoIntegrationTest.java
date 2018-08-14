36c51606218129cbc6b864f8bf96cc50fca5b213
brandontjq <brandontjq@gmail.com>
2018-08-14 14:36:56 +0800

    adding test case to orderrepotest

--- src/test/java/com/cs/test/dao/OrderRepoIntegrationTest.java
+++ src/test/java/com/cs/test/dao/OrderRepoIntegrationTest.java
@@ -3,6 +3,8 @@ package com.cs.test.dao;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +13,13 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import com.cs.Csteama2018Application;
 import com.cs.dao.OrderRepository;
+import com.cs.domain.Company;
+import com.cs.domain.Industry;
+import com.cs.domain.Order;
+import com.cs.domain.Role;
+import com.cs.domain.User;
+
+import static org.hamcrest.Matchers.samePropertyValuesAs;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = {Csteama2018Application.class})
@@ -24,5 +33,13 @@ public class OrderRepoIntegrationTest {
         assertThat(orderRepository.findAllOrders().size(), is(3));
     }
     
+    @Test
+    public void canFindByOrderId() {
+    	DateTimeFormatter formatter = DateTimeFormat.forPattern("dd/MM/yyyy HH:mm:ss");
+    	Industry industry = new Industry("IT Services","Services");
+    	Company company = new Company("ABC.HK","ABC CO HONG KONG", industry);
+    	User user = new User(1,"Jon","Doe", "1234","jondoe@gmail.com", Role.TRADER);
+    	assertThat(orderRepository.getOrder(1), samePropertyValuesAs(new Order(1,company,"B","LIMIT",10.0,5,formatter.parseDateTime("16/08/2018 10:17:23"),user,"OPENED")));
+    }
     
 }
